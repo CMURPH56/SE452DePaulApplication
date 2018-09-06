d6cd31c43c759b91d44b7a6fcc7cb1398fe0d4d1
CMURPH56 <CameronMurphy87@gmail.com>
2018-09-06 15:59:48 -0500

    first commit setup project

---
+++ src/main/java/edu/depaul/cdm/se/sampleproject/DePaulApplication.java
@@ -0,0 +1,12 @@
+package edu.depaul.cdm.se.sampleproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DePaulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DePaulApplication.class, args);
+	}
+}
